d3377c750bda692a22d6f2b44766ff4ca5a75be5
dilshodsherzodovich <dilshod.sherzodovich.2004@gmail.com>
2023-02-22 19:21:49 +0500

    more details added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,13 +3,13 @@ import { auth, provider } from "../../config";
 import { signInWithPopup } from "firebase/auth";
 import Login from "../Login/Login";
 
-function Home(props) {
+function Home() {
   const [value, setValue] = useState("");
 
   const handleClick = () => {
     signInWithPopup(auth, provider).then((data) => {
       console.log(data);
-      setValue(data.user.email);
+      setValue(data);
       localStorage.setItem("email", data.user.email);
     });
   };
@@ -19,7 +19,7 @@ function Home(props) {
 
   return (
     <div>
-      {value ? (
+      {value?.user?.email ? (
         <div>
           <h1>Home page</h1>
           <button
@@ -30,6 +30,8 @@ function Home(props) {
             class=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
             Log out
+            <h1>{value?.user?.email}</h1>
+            <h1>Name: {value?.user?.displayName}</h1>
           </button>
         </div>
       ) : (
